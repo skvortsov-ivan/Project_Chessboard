c7364f3d8a18aa2da32d5983a102a2d289d3ff70
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2025-09-12 10:51:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//När programmet startar ska användaren få mata in en siffra N (3–50). Validera inmatningen.
+//Skriv ut ett N×N "schackbräde" med så många rader och kolumner som användaren angav.
+//Brädet skriver du ut genom att låta varje svart ruta representeras av tecknet ◼︎ och varje vit ruta av tecknet ◻︎.
+namespace Project_Chessboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the amount of rows ");
+        }
+    }
+}
